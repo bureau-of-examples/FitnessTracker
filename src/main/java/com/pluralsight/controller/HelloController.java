8ded6d335fb7f517750d45a0d54adeb27ffb34c4
zhy2002 <hz141@uowmail.edu.au>
2015-06-23 13:47:08 +1000

    Tested @MatrixVariable.

--- src/main/java/com/pluralsight/controller/HelloController.java
+++ src/main/java/com/pluralsight/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.pluralsight.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.MatrixVariable;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.servlet.mvc.LastModified;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
+import java.util.Map;
 
 @Controller
 @RequestMapping
@@ -23,9 +25,19 @@ public class HelloController implements LastModified { //todo LastModified not w
     }
 
     @RequestMapping(value = {"/params/{id}"})
-    public String requestParams(@PathVariable("id") Integer id, @RequestHeader("User-Agent") String userAgent,  Model model) {
+    public String requestParams(@PathVariable("id") Integer id, @RequestHeader("User-Agent") String userAgent, /*doesn't work before extension*/@MatrixVariable(pathVar = "id") Map<String, String> matrixVariables,  Model model) {
         model.addAttribute("id", id);
         model.addAttribute("userAgent", userAgent);
+        model.addAttribute("matrixVariables", matrixVariables);
+
+        return "params";
+    }
+
+    @RequestMapping(value = {"/params/{id}/matrix"})
+    public String dumpMatrixVariables(@PathVariable("id") Integer id, /*works*/@MatrixVariable(pathVar = "id") Map<String, String> matrixVariables,  Model model){
+        model.addAttribute("id", id);
+        model.addAttribute("matrixVariables", matrixVariables);
+
         return "params";
     }
 
--- src/main/webapp/WEB-INF/jsp/params.jsp
+++ src/main/webapp/WEB-INF/jsp/params.jsp
@@ -6,6 +6,7 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
 <head>
     <title></title>
@@ -13,5 +14,15 @@
 <body>
        <p>The id is: [${id}]</p>
 <p>You are using [${userAgent}]</p>
+
+<div>
+    <h4>Matrix variables</h4>
+    <table style="border: 1px solid black">
+        <tr><td>Variable</td><td>Value</td></tr>
+        <c:forEach items="${matrixVariables}" var="entry" >
+            <tr><td>${entry.key}</td><td>${entry.value}</td></tr>
+        </c:forEach>
+    </table>
+</div>
 </body>
 </html>
