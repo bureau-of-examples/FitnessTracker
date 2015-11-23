b0b4cc8d3083ceffe6bc4db907fc70ad9d40740c
zhy2002 <hz141@uowmail.edu.au>
2015-11-23 13:23:57 +1100

    Configured integration testing with failsafe plugin.

---
+++ src/test/java/integration/IntegrationTestServices.java
@@ -0,0 +1,13 @@
+package integration;
+
+import org.junit.Test;
+
+public class IntegrationTestServices {
+
+    @Test
+    public void test(){
+
+        throw new RuntimeException("Dummy integration test set to fail");
+    }
+
+}
